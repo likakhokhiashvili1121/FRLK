e43361eb8f45d0c206d99c4648d47f4a85dcdd20
likakhokhiashvili1121 <likakhokhiashvili@gmail.com>
2020-12-11 01:00:11 +0400

    TIRED

---
+++ app/src/main/java/com/example/frlk/Fragments/ImageFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication133.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.frlk.R
+
+class ImageFragment:Fragment(R.layout.ragment_image) {
+}
---
+++ app/src/main/java/com/example/myapplication133/fragments/HomeFragmentDirections.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication133.fragments
+
+class HomeFragmentDirections {
+
+}
